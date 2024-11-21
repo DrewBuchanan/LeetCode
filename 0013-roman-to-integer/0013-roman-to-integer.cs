16c252a3d6bd5ae1278c3d045848986e10e0c0a7
DrewBuchanan <12664792+DrewBuchanan@users.noreply.github.com>
2024-11-21 15:18:39 -0600

    Time: 3 ms (60.50%), Space: 49.5 MB (29.03%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cs
+++ 0013-roman-to-integer/0013-roman-to-integer.cs
@@ -1,74 +1,28 @@
 public class Solution {
-    public int RomanToInt(string s) {
         
+	Dictionary<char, int> roman = new Dictionary<char, int>()
+	{
+		{ 'I', 1 },
+		{ 'V', 5 },
+		{ 'X', 10 },
+		{ 'L', 50 },
+		{ 'C', 100 },
+		{ 'D', 500 },
+		{ 'M', 1000 }
+	};
+
+	public int RomanToInt(string s)
+	{
 		int sum = 0;
-		for (int i = 0; i < s.Length; i++)
+		for (int i = 0; i < s.Length - 1; i++)
 		{
-			switch (s[i])
-			{
-				case 'I':
-					sum += 1;
-					if (i != s.Length - 1 && (s[i + 1] == 'V' || s[i + 1] == 'X'))
-					{
-						if (s[i + 1] == 'V')
-						{
-							sum += 3;
-							i++;
-						}
-						else if (s[i + 1] == 'X')
-						{
-							sum += 8;
-							i++;
-						}
-					}
-					break;
-				case 'V':
-					sum += 5;
-					break;
-				case 'X':
-					sum += 10;
-					if (i != s.Length - 1 && (s[i + 1] == 'L' || s[i + 1] == 'C'))
-					{
-						if (s[i + 1] == 'L')
-						{
-							sum += 30;
-							i++;
-						}
-						else if (s[i + 1] == 'C')
-						{
-							sum += 80;
-							i++;
-						}
-					}
-					break;
-				case 'L':
-					sum += 50;
-					break;
-				case 'C':
-					sum += 100;
-					if (i != s.Length - 1 && (s[i+1] == 'D' || s[i+1] == 'M'))
-					{
-						if (s[i + 1] == 'D')
-						{
-							sum += 300;
-							i++;
-						}
-						else if (s[i + 1] == 'M')
-						{
-							sum += 800;
-							i++;
-						}
-					}
-					break;
-				case 'D':
-					sum += 500;
-					break;
-				case 'M':
-					sum += 1000;
-						break;
-			}
+			if (roman[s[i]] < roman[s[i + 1]])
+				sum -= roman[s[i]];
+			else
+				sum += roman[s[i]];
 		}
 
-		return sum;
-    }
+		return sum + roman[s[s.Length - 1]];
+	}
+    
 }
