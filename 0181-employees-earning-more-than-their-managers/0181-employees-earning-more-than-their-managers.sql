78b3ae1788da5e48eaef9a56b58839654af51214
DrewBuchanan <12664792+DrewBuchanan@users.noreply.github.com>
2024-11-23 22:49:27 -0600

    Time: 430 ms (97.45%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,4 +1,4 @@
 /* Write your T-SQL query statement below */
 SELECT e1.name as Employee
 FROM Employee e1
-    INNER JOIN Employee e2 ON e1.managerId = e2.id AND e1.salary > e2.salary
+WHERE e1.salary > (SELECT salary FROM employee e2 WHERE e1.managerId = e2.id)
