f84d2c5a9f6df0015bf25d0174dadc7afd0836f7
DrewBuchanan <12664792+DrewBuchanan@users.noreply.github.com>
2024-11-26 21:45:29 -0600

    Time: 279 ms (79.29%), Space: 0B (100.00%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -2,13 +2,8 @@
 SELECT  x,
         y,
         z,
-        'Yes' as triangle
-FROM triangle
-WHERE x + y > z AND x + z > y AND y + z > x
-UNION
-SELECT  x,
-        y,
-        z,
-        'No' as triangle
-FROM triangle
-WHERE NOT (x + y > z AND x + z > y AND y + z > x);
+        CASE
+            WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+            ELSE 'No'
+        END as triangle
+FROM triangle;
