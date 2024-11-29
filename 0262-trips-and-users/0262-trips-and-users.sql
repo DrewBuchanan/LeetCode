f860b13700522120b2f59e48e718dd484d7ccc09
DrewBuchanan <12664792+DrewBuchanan@users.noreply.github.com>
2024-11-29 16:46:44 -0600

    Time: 399 ms (34.02%), Space: 0B (100.00%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,8 +1,20 @@
 /* Write your T-SQL query statement below */
+WITH unbanned_drivers AS
+(
+    SELECT users_id
+    FROM Users
+    WHERE banned = 'No' AND role = 'driver'
+),
+unbanned_clients AS
+(
+    SELECT users_id
+    FROM Users
+    WHERE banned = 'No' AND role = 'client'
+)
 SELECT  request_at AS 'Day',
-        CAST(ROUND(SUM(CASE WHEN t.status IN ('cancelled_by_driver', 'cancelled_by_client') THEN 1 ELSE 0 END) * 1.00 / COUNT(*), 2) AS NUMERIC(3,2)) AS 'Cancellation Rate'
+        CAST(ROUND(SUM(CASE WHEN t.status <> 'completed' THEN 1 ELSE 0 END) * 1.00 / COUNT(*), 2) AS NUMERIC(3,2)) AS 'Cancellation Rate'
 FROM Trips t
-    INNER JOIN Users d ON d.banned = 'No' AND d.role = 'driver' AND d.users_id = t.driver_id
-    INNER JOIN Users c ON c.banned = 'No' AND c.role = 'client' AND c.users_id = t.client_id
+    INNER JOIN unbanned_drivers d ON d.users_id = t.driver_id
+    INNER JOIN unbanned_clients c ON c.users_id = t.client_id
 WHERE CAST(t.request_at AS DATE) >= '10-01-2013' AND CAST(t.request_at AS DATE) <= '10-03-2013'
 GROUP BY request_at;
