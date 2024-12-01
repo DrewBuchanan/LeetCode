dbe9f646f904c4362a6f95fd332fee58a3756407
DrewBuchanan <12664792+DrewBuchanan@users.noreply.github.com>
2024-11-30 23:12:01 -0600

    Time: 553 ms (83.97%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,5 +1,12 @@
 /* Write your T-SQL query statement below */
+/*
 SELECT DISTINCT l.num as 'ConsecutiveNums'
 FROM Logs l
     INNER JOIN Logs m ON m.id = l.id + 1 AND m.num = l.num
-    INNER JOIN Logs n ON n.id = l.id + 2 AND n.num = l.num
+    INNER JOIN Logs n ON n.id = l.id + 2 AND n.num = l.num;
+    */
+    
+    
+SELECT DISTINCT num as 'ConsecutiveNums'
+FROM Logs l
+WHERE 2 <= (SELECT COUNT(*) FROM Logs WHERE l.num = num AND id IN (l.id + 1, l.id + 2))
