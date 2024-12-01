c417c9ec2da2800df3ab5f1f2a9b0c2a8c11d149
DrewBuchanan <12664792+DrewBuchanan@users.noreply.github.com>
2024-11-30 22:30:00 -0600

    Time: 5 ms (6.28%), Space: 44.3 MB (6.69%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cs
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cs
@@ -1,6 +1,7 @@
 public class Solution {
     public bool CheckIfExist(int[] arr) 
     {
+        /*
         for (int i = 0; i < arr.Length; i++)
         {
             for (int j = i + 1; j < arr.Length; j++)
@@ -10,5 +11,35 @@ public class Solution {
             }
         }
         return false;
+        */
+        
+        Array.Sort(arr);
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int index = CustomBinarySearch(arr, arr[i] * 2);
+            if (index >= 0 && index != i)
+                return true;
+        }
+        
+        return false;
+    }
+    
+    public int CustomBinarySearch(int[] arr, int target)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+        int mid = 0;
+        while (left <= right)
+        {
+            mid = left + (right - left) / 2;
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        
+        return -1;
     }
 }
