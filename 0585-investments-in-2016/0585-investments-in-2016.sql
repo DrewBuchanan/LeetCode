82b8fb8dc9a16290de248623ce6f8e21dbae79a6
DrewBuchanan <12664792+DrewBuchanan@users.noreply.github.com>
2024-12-02 20:54:15 -0600

    Time: 494 ms (98.99%), Space: 0B (100.00%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,11 @@
+WITH distinct_locations AS
+(
+	SELECT lat, lon
+	FROM insurance
+	GROUP BY lat, lon
+	HAVING COUNT(*) = 1
+)
+SELECT CAST(SUM(tiv_2016) as NUMERIC(18,2)) as 'tiv_2016'
+FROM Insurance i
+	INNER JOIN distinct_locations d ON i.lat = d.lat AND i.lon = d.lon
+WHERE EXISTS (SELECT * FROM Insurance j WHERE i.pid <> j.pid AND j.tiv_2015 = i.tiv_2015)
